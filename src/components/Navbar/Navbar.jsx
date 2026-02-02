59a8db494ad28aa1cebac8e30a9a6cbf90256a3c
Rohit patil <136801508+Rohitpatil34@users.noreply.github.com>
2026-02-02 19:53:04 +0530

    created navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import DesktopNavbar from "./DesktopNavbar";
+import MobileTopNavbar from "./MobileTopNav";
+import MobileBottomNavbar from "./MobileBottomNav";
+
+const Navbar = () => {
+  return (
+    <>
+      <DesktopNavbar />
+      <MobileTopNavbar />
+      
+    </>
+  );
+};
+
+export default Navbar;
