af7bd1ca7d0d57b03d6c0b3536e8b347bf4ea6fb
Rohit patil <136801508+Rohitpatil34@users.noreply.github.com>
2026-02-08 23:06:27 +0530

    added index.js(backend)

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import process from "process";
+import otproutes from "./routes/otproutes.js"
+
+import userRoutes from "./routes/userroutes.js";
+import gameRoutes from "./routes/gameroutes.js"
+import connectDB from "./Config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/otp", otproutes);
+app.use("/api/games", gameRoutes);
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running & DB connected");
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
