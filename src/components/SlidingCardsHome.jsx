501f179b4840eb479f63641572aba989b026cd30
Rohit patil <136801508+Rohitpatil34@users.noreply.github.com>
2026-02-24 22:58:10 +0530

    added

--- src/components/SlidingCardsHome.jsx
+++ src/components/SlidingCardsHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import image1 from "../assets/image1.png";
-import image2 from "../assets/image2.png";
-import image3 from "../assets/image3.png";
+import image1 from "../assets/Image1.png";
+import image2 from "../assets/Image2.png";
+import image3 from "../assets/Image3.png";
 import image4 from "../assets/image4.png";
 
 const SlidingCardsHome = () => {
